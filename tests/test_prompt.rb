35231905cf3655966cdbdbb714ea2a548225501a
Elhelw, Mohamed <114217025+melhelow@users.noreply.github.com>
2024-02-11 00:42:57 +0000

    fixed tests value

--- tests/test_prompt.rb
+++ tests/test_prompt.rb
@@ -5,14 +5,14 @@ class TestConvert < Minitest::Test
   def test_kg
     convert = Convert.new
     kilogram = 2
-    expected_result = kilogram * 2.20462
+    expected_result = 4.40924
     assert_equal(expected_result, convert.mult_input(kilogram), "Conversion from kg to lb method failed")
   end
 
   def test_lb
     convert = Convert.new
-    pound = 3
-    excpected_result = pound * 0.45359237
+    pound = 2
+    excpected_result = 0.90718474
     assert_equal(excpected_result, convert.div_input(pound), "Conversion from lb to kg method failed" )
   end
 end
